00de78fc21b946a222dcf5e9db1ab4dc0a39f4ba
aliyosif96 <118024937+aliyosif96@users.noreply.github.com>
2024-05-05 10:21:12 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Driver(models.Model):
     name = models.CharField(max_length=255)
     mobile_number = models.CharField(max_length=20)
